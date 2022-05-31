8e8a266827a2956cdc8772fd56fbee64e185293a
Duncan Murchison <duncancmurchison@gmail.com>
2022-05-31 16:22:45 -0400

    working on chartjs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Example from "./scripts/example";
+import myChart from "./scripts/nft_chart";
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log('Hello World');
   const main = document.getElementById("main")
   new Example(main)
+  const ctx = document.getElementById('myChart').getContext('2d');
 });
