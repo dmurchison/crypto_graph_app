b01ca9cfe6ea44ff8cc9c3da8353a0bac5ad0ceb
Duncan Murchison <duncancmurchison@gmail.com>
2022-05-31 13:23:04 -0400

    chartjs library

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -3,7 +3,7 @@ class Example {
     this.el = el;
     this.el.innerHTML = "<h1>It's ALIVE!</h1>"
 
-    this.el.addEventListener("click", this.handleClick.bind(this))
+    this.el.addEventListener('click', this.handleClick.bind(this))
   }
 
   handleClick() {
